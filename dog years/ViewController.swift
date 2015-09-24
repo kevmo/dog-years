290998037417e9faf6efa215000d5f401af1e09d
kevmo <k.james.moore@gmail.com>
2015-09-24 10:53:15 -0400

    Dog years

--- dog years/ViewController.swift
+++ dog years/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var dogAgeTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func findAge(sender: AnyObject) {
+        var dogAge = Int(dogAgeTextField.text!)!
+        dogAge = dogAge * 7
+        
+        resultLabel.text = "Your dog is \(dogAge) dog years old."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
